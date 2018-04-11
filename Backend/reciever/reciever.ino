591a644ddf91511f60ff4a64a1f6ca1a550395aa
Rowan D'Ausilio <rdausilio@gmail.com>
2018-04-11 11:28:48 -0400

    changed more stuff in the backend with receiver
    Lets hope this works cause I just spent 2 hours coding and changing things and I cant test it for another 7 hours!

--- Backend/reciever/reciever.ino
+++ Backend/reciever/reciever.ino
@@ -5,22 +5,21 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+RF24 radio(9, 10);
+const uint64_t pipe = 0xE6E6E6E6E6E6; //needs to be the same
+
 #define PIN 6
-
 #define NUM_LEDS 30
-
 #define BRIGHTNESS 40
-
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+int recievedMessage[1][3] = {000, 000, 000};
+
 int i;
 
-RF24 radio(9, 10); // CE, CSN
-const byte address[6] = "00001";
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
+  radio.openReadingPipe(1, pipe);
   radio.startListening();
 
   strip.setBrightness(BRIGHTNESS);
@@ -29,21 +28,11 @@ void setup() {
   i = 0;
 }
 void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+  delay(300);
+  while (radio.available()) {
+    radio.read(recievedMessage, 1);
+    for (int j = 0; j < NUM_LEDS; j++)
+      strip.setPixelColor(j, strip.Color(recievedMessage[1][1], recievedMessage[1][2], recievedMessage[1][3]));
+    strip.show();
   }
-
-  for (int j = 0; j < NUM_LEDS; j++)
-    strip.setPixelColor(j, strip.Color(0, 0, 0, 15));
-  strip.setPixelColor(i, strip.Color(66, 164, 244, 0));
-  strip.setPixelColor(i + 1, strip.Color(188, 33, 160, 0));
-  strip.setPixelColor(i + 2, strip.Color(66, 164, 244, 0));
-
-  strip.show();
-  delay(100);
-  if (++i >= NUM_LEDS)
-    i = 0;
-
 }
