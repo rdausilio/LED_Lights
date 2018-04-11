8d7f1621cec20f0bd0b76d8acb25034f1c0dd882
Rowan D'Ausilio <rdausilio@gmail.com>
2018-04-11 10:46:37 -0400

    Update webserv and pick_color
    does this actually work? whos to say

--- pick_color.py
+++ pick_color.py
@@ -1,4 +1,14 @@
+"""
+Rowan D'Ausilio
+LED Lightstrip project
+"""
+
 import serial
+
+"""
+debate on just a large switch statement for color change? 
+prefix b is required when writing to serial
+"""
 
 
 class Color_Picker():
@@ -9,26 +19,26 @@ class Color_Picker():
     def send(self, data):
         self.port.write(data.encode())
 
-    def red_color(self):
-        self.send('237, 28, 28')  # RGB - 237 28 28
+    def red_color(self, rv, gv, bv):
+        self.send("B" + rv + gv + bv)  # RGB - 237 28 28
 
-    def orange_color(self):
-        self.send('255, 126, 28')  # RGB - 255 126 28
+    def orange_color(self, rv, gv, bv):
+        self.send("B" + rv + gv + bv)  # RGB - 255 126 28
 
-    def yellow_color(self):
-        self.send('255, 228, 28')  # RGB - 255 228 28
+    def yellow_color(self, rv, gv, bv):
+        self.send("B" + rv + gv + bv)  # RGB - 255 228 28
 
-    def green_color(self):
-        self.send('42, 178, 37')  # RGB - 42 178 37
+    def green_color(self, rv, gv, bv):
+        self.send("B" + rv + gv + bv)  # RGB - 42 178 37
 
-    def blue_color(self):
-        self.send('24, 86, 178')  # RGB - 24 86 178
+    def blue_color(self, rv, gv, bv):
+        self.send("B" + rv + gv + bv)  # RGB - 24 86 178
 
-    def violet_color(self):
-        self.send('128, 18, 165')  # RGB - 128 18 165
+    def violet_color(self, rv, gv, bv):
+        self.send("B" + rv + gv + bv)  # RGB - 128 18 165
 
-    def custom_color(self, r_color, g_color, b_color):
-        self.send('r_color, g_color, b_color')
+    # def custom_color(self, r_color, g_color, b_color):
+    # self.send('r_color, g_color, b_color')
 
 
 if __name__ == '__main__':
@@ -37,3 +47,19 @@ if __name__ == '__main__':
         drive = Color_Picker(port)
     else:
         drive = Color_Picker()
+
+    while True:
+        choice = input("What color do you want the light strip to change to?")
+        choice = choice.upper()
+        if choice == "R":
+            drive.red_color(237, 28, 28)
+        if choice == "O":
+            drive.orange_color(225, 126, 28)
+        if choice == "Y":
+            drive.yellow_color(255, 228, 28)
+        if choice == "G":
+            drive.green_color(42, 178, 37)
+        if choice == "BLUE":
+            drive.blue_color(24, 86, 178)
+        if choice == "V":
+            drive.violet_color(128, 18, 165)
--- webserv.py
+++ webserv.py
@@ -1,11 +1,50 @@
-from flask import Flask, render_template
+"""
+Rowan D'Ausilio
+LED Lightstrip project
+"""
+
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/red')
+def red():
+    return jsonify(237, 28, 28)
+
+
+@app.route('/orange')
+def orange():
+    return jsonify(255, 126, 28)
+
+
+@app.route('/yellow')
+def yellow():
+    return jsonify(255, 228, 28)
+
+
+@app.route('/green')
+def green():
+    return jsonify(42, 178, 37)
+
+
+@app.route('/blue')
+def blue():
+    return jsonify(24, 86, 178)
+
+
+@app.route('/violet')
+def violet():
+    return jsonify(128, 18, 165)
+
+
+@app.route('/custom')
+def custom():
+    pass
 
 
 if __name__ == "__main__":
